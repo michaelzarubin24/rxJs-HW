f0ee00c0e614708639be0ac65872d4ae60af7b2e
michaelzarubin24 <129330244+michaelzarubin24@users.noreply.github.com>
2024-03-25 18:26:31 +0200

    hw done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, forkJoin, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'rxJs-HW';
+export class AppComponent implements OnInit {
+  resources: string[] = [
+    'posts',
+    'comments',
+    'albums',
+    'photos',
+    'todos',
+    'users',
+  ];
+  fetchedData: { endpoint: string; data: any; error: any }[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    const requests: Observable<any>[] = this.resources.map((endpoint) =>
+      this.dataService
+        .fetchData(endpoint)
+        .pipe(catchError((error) => throwError(() => ({ endpoint, error }))))
+    );
+
+    forkJoin(requests).subscribe({
+      next: (data) => {
+        this.fetchedData = data.map((response, index) => ({
+          endpoint: this.resources[index],
+          data: response,
+          error: null,
+        }));
+      },
+      error: (err) => {
+        this.fetchedData.push({
+          endpoint: err.endpoint,
+          data: null,
+          error: err.error,
+        });
+      },
+    });
+  }
 }
